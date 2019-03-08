7fe554c1550e391a57b8ead718072caad776bd84
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-09 00:41:52 +0100

    New agent based on alpine and sshd. Connecting from master->agent with ssh.

--- java-agent/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT} -t 300
-bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_JNLP_PORT} -t 300
-
-java -jar slave.jar -jnlpUrl http://${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp -secret ${JENKINS_TOKEN} -workDir /var/data

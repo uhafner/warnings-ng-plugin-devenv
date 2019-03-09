9af66a16306329e5fd641615777d59c6ebd4aa36
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-09 23:38:00 +0100

    Fixed git path.

--- clone-repos.sh
+++ clone-repos.sh
@@ -6,8 +6,12 @@ if [[ -z "$JENKINS_HOME" ]]; then
 fi
 
 echo Clone of Jenkins plugins using SSH and your GitHub key
+
 git clone git@github.com:jenkinsci/analysis-model.git
 git clone git@github.com:jenkinsci/analysis-model-api-plugin.git
 git clone git@github.com:jenkinsci/warnings-ng-plugin.git
 
 git clone git@github.com:jenkinsci/acceptance-test-harness.git
+
+echo Done cloning. Note that you need to change the remotes for each
+echo of the repositories you want to contribute to. 

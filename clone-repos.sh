70284a6be07ce6b09a92a64dd17250104a55b73b
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-10 11:14:25 +0100

    Do not depend on JENKINS_HOME.

--- clone-repos.sh
+++ clone-repos.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ -z "$JENKINS_HOME" ]]; then
-    echo "JENKINS_HOME is not defined" 1>&2
-    exit 1
-fi
-
 echo Clone of Jenkins plugins using SSH and your GitHub key
 
 git clone git@github.com:jenkinsci/analysis-model.git
@@ -14,4 +9,4 @@ git clone git@github.com:jenkinsci/warnings-ng-plugin.git
 git clone git@github.com:jenkinsci/acceptance-test-harness.git
 
 echo Done cloning. Note that you need to change the remotes for each
-echo of the repositories you want to contribute to. 
+echo of the repositories you want to contribute to.

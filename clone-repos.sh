28f2f1ff65259f94e5f2c9df4dd37f3818910915
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-10 11:41:17 +0100

    Added coloring of scripts.

--- clone-repos.sh
+++ clone-repos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo Clone of Jenkins plugins using SSH and your GitHub key
+mark="$(tput setaf 2)$(tput bold)"
+bold="$(tput bold)"
+norm="$(tput setaf 0)$(tput sgr0)"
+
+echo ${mark}${bold}Clone of Jenkins plugins using SSH and your GitHub key
+read -n 1 -s -r -p "${norm}Press any key to continue and compile the projects"
 
 git clone git@github.com:jenkinsci/analysis-model.git || { echo "Clone failed"; exit 1; }
 git clone git@github.com:jenkinsci/analysis-model-api-plugin.git || { echo "Clone failed"; exit 1; }
@@ -8,9 +13,9 @@ git clone git@github.com:jenkinsci/warnings-ng-plugin.git || { echo "Clone faile
 
 git clone git@github.com:jenkinsci/acceptance-test-harness.git || { echo "Clone failed"; exit 1; }
 
-echo Done cloning. Note that you need to change the remotes for each
+echo ${mark}${bold}Done cloning. Note that you need to change the remotes for each
 echo of the repositories you want to contribute to.
 
-read -n 1 -s -r -p "Press any key to continue and compile the projects"
+read -n 1 -s -r -p "${norm}Press any key to continue and compile the projects"
 
 mvn -V -U -e install -DskipTests

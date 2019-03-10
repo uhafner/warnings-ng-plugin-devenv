0add0ee36d838af588f2a29bedaadcc83264bd5d
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-10 11:15:54 +0100

    Added error handling during clone.

--- clone-repos.sh
+++ clone-repos.sh
@@ -2,11 +2,11 @@
 
 echo Clone of Jenkins plugins using SSH and your GitHub key
 
-git clone git@github.com:jenkinsci/analysis-model.git
-git clone git@github.com:jenkinsci/analysis-model-api-plugin.git
-git clone git@github.com:jenkinsci/warnings-ng-plugin.git
+git clone git@github.com:jenkinsci/analysis-model.git || { echo "Clone failed"; exit 1; }
+git clone git@github.com:jenkinsci/analysis-model-api-plugin.git || { echo "Clone failed"; exit 1; }
+git clone git@github.com:jenkinsci/warnings-ng-plugin.git || { echo "Clone failed"; exit 1; }
 
-git clone git@github.com:jenkinsci/acceptance-test-harness.git
+git clone git@github.com:jenkinsci/acceptance-test-harness.git || { echo "Clone failed"; exit 1; }
 
 echo Done cloning. Note that you need to change the remotes for each
 echo of the repositories you want to contribute to.

0d67297965ace0fb878bb69d8666f7f94efb3e51
Ulli Hafner <ullrich.hafner@gmail.com>
2019-09-30 19:42:22 +0200

    Remove jenkins-war folder.

--- start-jut.sh
+++ start-jut.sh
@@ -4,20 +4,23 @@ error="$(tput setaf 1)$(tput bold)"
 warn="$(tput setaf 3)$(tput bold)"
 ok="$(tput setaf 2)$(tput bold)"
 
-bold="$(tput bold)"
 norm="$(tput setaf 0)$(tput sgr0)"
 
-export JENKINS_HOME=$(cd ./docker/volumes/jenkins-home; pwd)
-export PLUGINS_DIR=$JENKINS_HOME/plugins
+# shellcheck disable=SC2164
+JENKINS_HOME=$(cd ./docker/volumes/jenkins-home; pwd)
+export JENKINS_HOME
 
+export PLUGINS_DIR=$JENKINS_HOME/plugins
 if [ ! -d $PLUGINS_DIR ]; then
     echo "${error}No Jenkins plugins found in ${PLUGINS_DIR}${norm}"
     exit 1;
 fi
+
 echo "${ok}Running Jenkins with plugins in ${PLUGINS_DIR}"
 
-export JENKINS_WAR="$(cd ./jenkins-war/; pwd)/jenkins.war"
-
+# shellcheck disable=SC2164
+JENKINS_WAR="$(cd ./acceptance-test-harness/; pwd)/jenkins.war"
+export JENKINS_WAR
 if [ ! -f $JENKINS_WAR ]; then
     echo "${warn}Jenkins war does not exist at ${JENKINS_WAR}"
     echo "${norm}Downloading latest LTS..."
@@ -28,5 +31,6 @@ echo "${ok}Using Jenkins under test ${JENKINS_WAR}${norm}"
 
 export WORKSPACE=/tmp
 
-cd acceptance-test-harness
+cd acceptance-test-harness || exit 1;
+
 ./jut-server.sh -n 1

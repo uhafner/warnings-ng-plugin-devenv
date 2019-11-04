fdc893a70dc1d0a4fc0c73b2f541af30d2c2615c
Ulli Hafner <ullrich.hafner@gmail.com>
2019-11-04 23:46:51 +0100

    Finished restructuring of JS libraries to plugins.

---
+++ go.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ./bootstrap4-api-plugin
+./go.sh
+cd ..
+
+cd ./data-tables-api-plugin
+./go.sh
+cd ..
+
+cd ./echarts-api-plugin
+./go.sh
+cd ..
+
+cd ./font-awesome-api-plugin
+./go.sh
+cd ..
+
+cd ./jquery3-api-plugin
+./go.sh
+cd ..
+
+cd ./popper-api-plugin
+./go.sh
+cd ..
+
+cd ./warnings-ng-plugin
+./go.sh
+cd ..

ece10d48c5edb525b560e5dbf0bb6bbaeb18afd3
Ulli Hafner <ullrich.hafner@gmail.com>
2021-03-21 13:33:08 +0100

    Ignore git plugin due to new Jenkins version constraint.

--- sync-configuration.sh
+++ sync-configuration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
+#find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
 
 #find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
 #find . -name .github -exec cp -vR etc/github/* {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
-#find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
+find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;

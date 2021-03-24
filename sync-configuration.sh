8ec9278def0a1946402ceba6d2d5a3ab2b5d7c2f
Ulli Hafner <ullrich.hafner@gmail.com>
2021-03-24 14:46:24 +0100

    Ignore token-macro > 2.13.

--- sync-configuration.sh
+++ sync-configuration.sh
@@ -5,5 +5,5 @@
 #find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
 #find . -name .github -exec cp -vR etc/github/* {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
-#find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
-find . -name assign-pr.yml -exec cp -vR etc/github/workflows/assign-pr.yml {} \;
+find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
+#find . -name assign-pr.yml -exec cp -vR etc/github/workflows/assign-pr.yml {} \;

30ab67237e2d1ce04a1652d0d80ba5bd3628ce6a
Ulli Hafner <ullrich.hafner@gmail.com>
2022-04-13 21:08:23 +0200

    Use `docker compose` rather than `docker-compose`.

--- jenkins.sh
+++ jenkins.sh
@@ -2,10 +2,10 @@
 
 docker pull jenkins/jenkins:alpine
 
-docker-compose build
+docker compose build
 
 # Make sure that docker runs with the current UID to avoid permission problems on volume docker/volumes/jenkins-home
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
 echo Running docker compose with user ID $CURRENT_UID
-docker-compose up
+docker compose up

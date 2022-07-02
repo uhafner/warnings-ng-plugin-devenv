59d20d27ca7886469e92d75c86e5fe51c5a603dd
Ulli Hafner <ullrich.hafner@gmail.com>
2022-07-02 21:35:07 +0200

    Use native `docker compose` rather than `docker-compose`.

--- jenkins.sh
+++ jenkins.sh
@@ -2,12 +2,11 @@
 
 docker pull jenkins/jenkins:alpine
 
-docker compose build
+docker compose build --pull
 
 # Make sure that docker runs with the current UID to avoid permission problems on volume docker/volumes/jenkins-home
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
 echo Running docker compose with user ID $CURRENT_UID
 
-#docker compose up seems not to work in the same way, Jenkins will not be recreated
-docker-compose up --always-recreate-deps
+docker compose up --always-recreate-deps

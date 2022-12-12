62f877c033eaef3633f3b429f9d218793818f5b5
Ulli Hafner <ullrich.hafner@gmail.com>
2022-12-12 21:41:14 +0100

    Make GitHub actions robust for branch names master and main.

--- sync-configuration.sh
+++ sync-configuration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
-find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
+find . -name workflows -exec cp -vR etc/github/workflows/* {} \;
+
+#find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
+#find . -name ci.yml -exec cp -vR etc/github/workflows/ci.yml {} \;
 
 #find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
-#find . -name .github -exec cp -vR etc/github/* {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
 #find . -name .github -exec cp -vR etc/github/release-drafter.yml {} \;

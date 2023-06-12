bf012363f4e420312e19b55c4d9d6f7b4c706b21
Ulli Hafner <ullrich.hafner@gmail.com>
2023-06-12 21:48:59 +0200

    Replace label checker with other GitHub action.

--- sync-configuration.sh
+++ sync-configuration.sh
@@ -8,7 +8,7 @@
 
 #find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
-find . -name workflows -exec cp -vR etc/github/workflows/run-release-drafter.yml {} \;
+find . -name workflows -exec cp -vR etc/github/workflows/enforce-labels.yml {} \;
 #find . -name .github -exec cp -vR etc/github/release-drafter.yml {} \;
 #find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
 #find . -name assign-pr.yml -exec cp -vR etc/github/workflows/assign-pr.yml {} \;

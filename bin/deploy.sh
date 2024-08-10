5f46950200c7d1cd0091f81c8b80d4214f440b85
Ulli Hafner <ullrich.hafner@gmail.com>
2024-08-10 18:16:36 +0200

    Use correct service name for docker compose.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,7 +16,7 @@ cp -fv warnings-ng-plugin/plugin/target/*hpi $JENKINS_HOME/plugins/warnings-ng.j
 
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
-IS_RUNNING=$(docker compose ps -q devenv-jenkins)
+IS_RUNNING=$(docker compose ps -q jenkins)
 if [[ "$IS_RUNNING" != "" ]]; then
     docker compose restart
     echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."

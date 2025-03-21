e31ca46f82f99e4965909acd58fcd638064b8c19
Ulli Hafner <ullrich.hafner@gmail.com>
2025-03-21 17:35:09 +0100

    Use umbrelladocs/action-linkspector to check Markdown and AsciiDoc links

--- bin/sync-configuration.sh
+++ bin/sync-configuration.sh
@@ -12,7 +12,8 @@
 #find . -name .github -exec cp -vR etc/github/release-drafter.yml {} \;
 #find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
 #find . -name assign-pr.yml -exec cp -vR etc/github/workflows/assign-pr.yml {} \;
-#find . -name check-md-links.yml -exec cp -vR etc/github/workflows/check-md-links.yml {} \;
+find . -name check-md-links.yml -exec cp -vR etc/github/workflows/check-md-links.yml {} \;
+find . -name linkspector.yml -exec cp -vR etc/github/linkspector.yml {} \;
 #find . -name enforce-labels.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/enforce-labels.yml {} \;
 #find . -name run-release-drafter.yml -exec cp -vR etc/github/workflows/run-release-drafter.yml {} \;
 #find . -name sync-labels.yml -exec cp -vR etc/github/workflows/sync-labels.yml {} \;
@@ -24,5 +25,5 @@
 #find . -name coverage.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/coverage.yml {} \;
 #find . -name quality-monitor.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor.yml {} \;
 #find . -name update-badges.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/update-badges.yml {} \;
-find . -name quality-monitor-pit.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor-pit.yml {} \;
-find . -name quality-monitor.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor.yml {} \;
+#find . -name quality-monitor-pit.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor-pit.yml {} \;
+#find . -name quality-monitor.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor.yml {} \;

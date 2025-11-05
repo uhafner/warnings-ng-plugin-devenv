f52fd202927875dde283699a340dee6ca6c8b538
Ulli Hafner <ullrich.hafner@gmail.com>
2025-11-05 16:42:23 +0100

    Build project only with Java 21 and 25

--- bin/sync-configuration.sh
+++ bin/sync-configuration.sh
@@ -5,14 +5,13 @@
 #find .  -maxdepth 4 -name autograding.yml -exec rm -i {} \;
 #find . -name .github -exec cp -vR etc/github/check-md-links.json {} \;
 
-find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
+#find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
 #find . -name .github -exec cp -vR etc/github/labels.yml {} \;
 #find . -name workflows -exec cp -vR etc/github/workflows/enforce-labels.yml {} \;
 #find . -name workflows -exec cp -vR etc/github/workflows/codeql.yml {} \;
 #find . -name .github -exec cp -vR etc/github/release-drafter.yml {} \;
 #find . -name .github -exec cp -vR etc/github/dependabot.yml {} \;
 #find . -name assign-pr.yml -exec cp -vR etc/github/workflows/assign-pr.yml {} \;
-#find . -name check-md-links.yml -exec cp -vR etc/github/workflows/check-md-links.yml {} \;
 #find . -name linkspector.yml -exec cp -vR etc/github/linkspector.yml {} \;
 #find . -name enforce-labels.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/enforce-labels.yml {} \;
 #find . -name run-release-drafter.yml -exec cp -vR etc/github/workflows/run-release-drafter.yml {} \;
@@ -20,11 +19,12 @@ find . -name assertj-templates -exec cp -v etc/assertj-templates/* {} \;
 
 #find . -maxdepth 4 -path '*plugin*autograding.yml' -exec rm -f {} \;
 
-#find . -name ci.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/ci.yml {} \;
-#find . -name codeql.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/codeql.yml {} \;
+find . -name check-md-links.yml -exec cp -vR etc/github/workflows/check-md-links.yml {} \;
+find . -name ci.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/ci.yml {} \;
+find . -name codeql.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/codeql.yml {} \;
+find . -name quality-monitor-build.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor-build.yml {} \;
 #find . -name coverage.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/coverage.yml {} \;
-#find . -name quality-monitor-build.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor-build.yml {} \;
-#find . -name update-badges.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/update-badges.yml {} \;
+find . -name update-badges.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/update-badges.yml {} \;
 # find . -name quality-monitor-pit.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor-pit.yml {} \;
 # find . -name quality-monitor.yml -prune -not -regex '.*target.*' -exec cp -vR etc/github/workflows/quality-monitor.yml {} \;
 
